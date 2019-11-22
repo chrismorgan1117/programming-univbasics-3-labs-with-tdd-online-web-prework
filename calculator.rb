0039bb802821c5ff0b75a130d52f575c37e85744
Christopher Morgan <cwm111794@gmail.com>
2019-11-22 03:00:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,9 @@
 # Add your variables here
- first_number = rand()
- second_number = 33
+ first_number = rand(0..1000)
+ second_number = rand(0..1000)
  sum = first_number + second_number
+ difference = first_number - second_number
+ 
  
  
  
