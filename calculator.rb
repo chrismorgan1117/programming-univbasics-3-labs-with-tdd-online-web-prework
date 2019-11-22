7cb504053f05460e71878d4813e49f15403a3404
Christopher Morgan <cwm111794@gmail.com>
2019-11-22 03:06:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,10 @@
 # Add your variables here
  first_number = rand
  second_number = rand
- sum = first_number + second_number
- difference = first_number - second_number
- product = first_number * second_number
- quotient = first_number / second_number
+ sum = (first_number + second_number)
+ difference = (first_number - second_number)
+ product = (first_number * second_number)
+ quotient = (first_number / second_number)
  
  
  
