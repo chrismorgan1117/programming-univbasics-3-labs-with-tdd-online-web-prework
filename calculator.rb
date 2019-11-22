15c8a8078575f079721f4f2c2b8d78119bc10af6
Christopher Morgan <cwm111794@gmail.com>
2019-11-22 03:03:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
- first_number = rand(0..1000)
- second_number = rand(0..1000)
+ first_number = 14
+ second_number = 12
  sum = first_number + second_number
  difference = first_number - second_number
  
